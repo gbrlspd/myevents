686e2351d6e8f2c728d14aeba3598126b8ad0fa5
Gabriel Spada <gabrielspadadesouza@gmail.com>
2023-02-16 15:41:43 -0300

    Add SQLite

--- Backend/src/MyEvents.API/Controllers/EventController.cs
+++ Backend/src/MyEvents.API/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MyEvents.API.Data;
 using MyEvents.API.Models;
 
 namespace MyEvents.API.Controllers;
@@ -7,39 +8,21 @@ namespace MyEvents.API.Controllers;
 [Route("api/[controller]")]
 public class EventController : ControllerBase
 {
-    public IEnumerable<Event> _event = new Event[] {
-        new Event() {
-            EventID = 1,
-            Place = "Piracicaba",
-            Date = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
-            Theme = "Test Event",
-            PeopleQty = 175,
-            Batch = "1st Batch"
-        },
-        new Event() {
-            EventID = 2,
-            Place = "Limeira",
-            Date = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
-            Theme = "Test Event 2",
-            PeopleQty = 180,
-            Batch = "1st Batch"
-        }
-    };
-    
-    public EventController()
+    private readonly DataContext context;
+    public EventController(DataContext context)
     {
-
+      this.context = context;
     }
 
     [HttpGet]
     public IEnumerable<Event> Get()
     {
-        return _event;
+        return context.Events;
     }
     
     [HttpGet("{id}")]
-    public IEnumerable<Event> GetByID(int id)
+    public Event GetByID(int id)
     {
-        return _event.Where(e => e.EventID == id);
+        return context.Events.FirstOrDefault(e => e.EventID == id);
     }
 }
