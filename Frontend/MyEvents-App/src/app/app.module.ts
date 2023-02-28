4676ea98eb8173fbc749bb6ed3833abb67e4cd24
Gabriel Spada <gabrielspadadesouza@gmail.com>
2023-02-28 15:25:52 -0300

    Add event list page

--- Frontend/MyEvents-App/src/app/app.module.ts
+++ Frontend/MyEvents-App/src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { SpeakersComponent } from './speakers/speakers.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsComponent, SpeakersComponent],
+  declarations: [
+    AppComponent,
+    EventsComponent,
+    SpeakersComponent,
+    NavComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
     TooltipModule.forRoot(),
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- Frontend/MyEvents-App/src/app/events/events.component.ts
+++ Frontend/MyEvents-App/src/app/events/events.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
-  public events: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+
+  imgWidth: number = 160;
+  showImg: boolean = true;
+
+  private _listFilter: string = '';
+
+  public get listFilter(): string {
+    return this._listFilter;
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredEvents = this.listFilter
+      ? this.filterEvents(this.listFilter)
+      : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (event: { theme: string; place: string }) =>
+        event.theme.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        event.place.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -15,9 +41,16 @@ export class EventsComponent implements OnInit {
     this.getEvents();
   }
 
+  toggleImg() {
+    this.showImg = !this.showImg;
+  }
+
   public getEvents(): void {
     this.http.get('https://localhost:7178/api/events').subscribe(
-      (res) => (this.events = res),
+      (res) => {
+        this.events = res;
+        this.filteredEvents = this.events;
+      },
       (err) => console.log(err)
     );
   }
